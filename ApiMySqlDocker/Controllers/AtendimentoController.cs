1a80603cdd27519cc9f029d4e53b2f0863865945
Thiago Alves <45457044+ThiaAlves@users.noreply.github.com>
2022-11-30 10:02:00 -0600

    Update AtendimentoController.cs

--- ApiMySqlDocker/Controllers/AtendimentoController.cs
+++ ApiMySqlDocker/Controllers/AtendimentoController.cs
@@ -35,6 +35,12 @@ namespace ApiMySqlDocker.Controllers
                 .Include(a => a.Medico)
                 .Include(a => a.Clinica)
                 .ToListAsync();
+                
+            foreach (var atendimento in atendimentos)
+            {
+                atendimento.Cliente.Cpf = Criptografia.AesDecrypt(atendimento.Cliente.Cpf);
+                atendimento.Cliente.Religiao = Criptografia.AesDecrypt(atendimento.Cliente.Religiao);
+            }
 
             return atendimentos;
             //return await _context.Atendimentos.ToListAsync();
@@ -117,6 +123,12 @@ namespace ApiMySqlDocker.Controllers
                 .Include(a => a.Clinica)
                 .Where(a => a.ClinicaId == id)
                 .ToListAsync();
+                
+            foreach (var atendimento in atendimentos)
+            {
+                atendimento.Cliente.Cpf = Criptografia.AesDecrypt(atendimento.Cliente.Cpf);
+                atendimento.Cliente.Religiao = Criptografia.AesDecrypt(atendimento.Cliente.Religiao);
+            }
 
             return atendimentos;
         }
